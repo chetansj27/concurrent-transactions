b7a08ec3f72d63918a60819fa5f45f7c7f955e09
Chetan <54722351+chetansj27@users.noreply.github.com>
2026-01-05 22:29:24 +0530

    first commit

---
+++ src/main/java/com/example/concurrent_transactions/ConcurrentTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrent_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/concurrent_transactions/ConcurrentTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrent_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
